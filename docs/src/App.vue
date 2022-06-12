7e2a656b34fedfb3e2e762133ec449e88067ad85
flkt <flkt.crnpio@gmail.com>
2022-06-12 10:25:14 -0500

    cambiando script al estilo vue3. agregando contendores a la navegacion y menu

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -1,46 +1,44 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-</script>
+import { ref } from 'vue'
 
-<script>
-export default {
-  data: ()=> ({
-    tipografiaAccesible: false,
-    vistaSimplificada: false,
-  }),
-  methods: {
-    cambiarTipografia() {
-      this.tipografiaAccesible = !this.tipografiaAccesible
-    },
-    cambiarVista() {
-      this.vistaSimplificada = !this.vistaSimplificada
-    },
-  }
+const isA11yTypography = ref(0)
+const isA11yView = ref(0)
+
+isA11yTypography.value = false
+isA11yView.value = false
+
+function toggleA11yTypography() {
+  isA11yTypography.value = !isA11yTypography.value
+}
+
+function toggleA11yView() {
+  isA11yView.value = !isA11yView.value
 }
 </script>
 
 <template>
-  <div :class="{ 'a11y-tipografia':tipografiaAccesible, 'a11y-simplificada':vistaSimplificada }">
-    <nav>
+  <div :class="{ 'a11y-tipografia':isA11yTypography, 'a11y-simplificada':isA11yView }">
+    <nav role="navigation" class="contenedor p-y-1">
       Navegacion principal
-      <RouterLink to="/">Inicio</RouterLink>
-      <RouterLink to="/reticula">Reticula</RouterLink>
-      <RouterLink to="/color">Color</RouterLink>
-      <RouterLink to="/tipografia">Tipografia</RouterLink>
-      <RouterLink to="/iconografia">Iconografia</RouterLink>
-      <RouterLink to="/botones">Botones</RouterLink>
-      <RouterLink to="/hipervinculos">Hipervínculos</RouterLink>
+      <RouterLink class="m-x-1" to="/">Inicio</RouterLink>
+      <RouterLink class="m-x-1" to="/reticula">Reticula</RouterLink>
+      <RouterLink class="m-x-1" to="/color">Color</RouterLink>
+      <RouterLink class="m-x-1" to="/tipografia">Tipografia</RouterLink>
+      <RouterLink class="m-x-1" to="/iconografia">Iconografia</RouterLink>
+      <RouterLink class="m-x-1" to="/botones">Botones</RouterLink>
+      <RouterLink class="m-x-1" to="/hipervinculos">Hipervínculos</RouterLink>
     </nav>
-    <menu>
+    <aside role="complementary" class="contenedor p-y-1">
       a11y
-      <button class="boton-primario" @click="cambiarTipografia">Cambiar tipografia</button>
-      <button class="boton-primario" @click="cambiarVista">Cambiar vista</button>
-    </menu>
-    <RouterView />
+      <button class="boton-primario" @click="toggleA11yTypography">Cambiar tipografia</button>
+      <button class="boton-primario" @click="toggleA11yView">Cambiar vista</button>
+    </aside>
+    <main role="main">
+      <RouterView />
+    </main>
   </div>
-  <!-- <body class="a11y-tipografia"> -->
-  <!-- <body class="a11y-simplificada"> -->
-  
+
 </template>
 
 <style>
