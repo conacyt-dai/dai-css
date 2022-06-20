329f5ea2c0d5a21850708d178f1b4e9d6d6ac6b0
flkt <flkt.crnpio@gmail.com>
2022-06-20 14:27:38 -0500

    arreglando estilos links identidad

--- docs/src/views/HipervinculosView.vue
+++ docs/src/views/HipervinculosView.vue
@@ -7,5 +7,14 @@
       <a href="#">a link <span class="icon dai-descargar"></span></a>
       <button class="hipervinculo">button.hipervinculo <span class="icon dai-descargar"></span></button>
     </p>
+    <h2>Identidad</h2>
+    <p class="fondo-gobmx-pino p-3">
+      <a href="#" class="hipervinculo-gobmx m-r-3">a.hipervinculo-gobmx</a>
+      <button class="hipervinculo-gobmx">button.hipervinculo-gobmx</button>
+    </p>
+    <p class="fondo-conacyt-azul p-3">
+      <a href="#" class="hipervinculo-conacyt m-r-3">a.hipervinculo-conacyt</a>
+      <button class="hipervinculo-conacyt">button.hipervinculo-conacyt</button>
+    </p>
   </div>
 </template>
