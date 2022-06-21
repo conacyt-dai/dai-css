2d65715b6cc3927d152ec9344cd14252cf3fa21c
flkt <flkt.crnpio@gmail.com>
2022-06-21 12:19:15 -0500

    revision colores identidad

--- docs/src/views/ColorView.vue
+++ docs/src/views/ColorView.vue
@@ -3,166 +3,184 @@
     <h1>Color</h1>
     <h2>Identidad Gobierno de México</h2>
     <div class="grid">
-      <div class="grid-column-1 m-y-1 fondo-gobmx-verde" style="border-radius: 8px;"></div>
+      <div class="grid-column-1 m-y-1 gobmx-fondo-oscuro-1" style="border-radius: 8px;"></div>
       <div class="grid-column-3">
-        <p>.fondo-gobmx-verde</p>
-        <p>#1F5D50</p>
-      </div>
-
-      <div class="grid-column-1 m-y-1 fondo-gobmx-pino" style="border-radius: 8px;"></div>
-      <div class="grid-column-3">
-        <p>.fondo-gobmx-pino</p>
+        <p>.gobmx-fondo-oscuro-1</p>
         <p>#18302D</p>
       </div>
-
-      <div class="grid-column-1 m-y-1 fondo-gobmx-beige" style="border-radius: 8px;"></div>
+      <div class="grid-column-1 m-y-1 gobmx-fondo-oscuro-2" style="border-radius: 8px;"></div>
       <div class="grid-column-3">
-        <p>.fondo-gobmx-beige</p>
+        <p>.gobmx-fondo-oscuro-2</p>
+        <p>#103e34</p>
+      </div>
+      <div class="grid-column-1 m-y-1 gobmx-fondo-claro-1" style="border-radius: 8px;"></div>
+      <div class="grid-column-3">
+        <p>.gobmx-fondo-claro-1</p>
+        <p>#fff</p>
+      </div>
+      <div class="grid-column-1 m-y-1 gobmx-fondo-claro-2" style="border-radius: 8px;"></div>
+      <div class="grid-column-3">
+        <p>.gobmx-fondo-claro-2</p>
         <p>#DFC9A1</p>
       </div>
-
-      <div class="grid-column-1 m-y-1 fondo-gobmx-dorado" style="border-radius: 8px;"></div>
+      <div class="grid-column-1 m-y-1 gobmx-fondo-acento" style="border-radius: 8px;"></div>
       <div class="grid-column-3">
-        <p>.fondo-gobmx-dorado</p>
-        <p>#BD9558</p>
-      </div>
-
-      <div class="grid-column-1 m-y-1 fondo-gobmx-tinto" style="border-radius: 8px;"></div>
-      <div class="grid-column-3">
-        <p>.fondo-gobmx-tinto</p>
+        <p>.gobmx-fondo-acento</p>
         <p>#9E2148</p>
       </div>
     </div>
-
     <div class="grid">
-      <div class="grid-column-1 m-y-1 borde-gobmx-verde" style="border-width: 4px; border-style: solid; border-radius: 8px;"></div>
+      <div class="grid-column-1 m-y-1 gobmx-borde-oscuro-1" style="border-width: 4px; border-style: solid; border-radius: 8px;"></div>
       <div class="grid-column-3">
-        <p>.borde-gobmx-verde</p>
-        <p>#1F5D50</p>
+        <p>.gobmx-borde-oscuro-1</p>
       </div>
-
-      <div class="grid-column-1 m-y-1 borde-gobmx-pino" style="border-width: 4px; border-style: solid; border-radius: 8px;"></div>
+      <div class="grid-column-1 m-y-1 gobmx-borde-oscuro-2" style="border-width: 4px; border-style: solid; border-radius: 8px;"></div>
       <div class="grid-column-3">
-        <p>.borde-gobmx-pino</p>
-        <p>#18302D</p>
+        <p>.gobmx-borde-oscuro-2</p>
       </div>
-
-      <div class="grid-column-1 m-y-1 borde-gobmx-beige" style="border-width: 4px; border-style: solid; border-radius: 8px;"></div>
+      <div class="grid-column-1 m-y-1 gobmx-borde-claro-1" style="border-width: 4px; border-style: solid; border-radius: 8px;"></div>
       <div class="grid-column-3">
-        <p>.borde-gobmx-beige</p>
-        <p>#DFC9A1</p>
+        <p>.gobmx-borde-claro-1</p>
       </div>
-
-      <div class="grid-column-1 m-y-1 borde-gobmx-dorado" style="border-width: 4px; border-style: solid; border-radius: 8px;"></div>
+      <div class="grid-column-1 m-y-1 gobmx-borde-claro-2" style="border-width: 4px; border-style: solid; border-radius: 8px;"></div>
       <div class="grid-column-3">
-        <p>.borde-gobmx-dorado</p>
-        <p>#BD9558</p>
+        <p>.gobmx-borde-claro-2</p>
       </div>
-
-      <div class="grid-column-1 m-y-1 borde-gobmx-tinto" style="border-width: 4px; border-style: solid; border-radius: 8px;"></div>
+      <div class="grid-column-1 m-y-1 gobmx-borde-acento" style="border-width: 4px; border-style: solid; border-radius: 8px;"></div>
       <div class="grid-column-3">
-        <p>.borde-gobmx-tinto</p>
-        <p>#9E2148</p>
+        <p>.gobmx-borde-acento</p>
       </div>
     </div>
-
     <div class="grid">
-      <div class="grid-column-1 m-y-1 texto-gobmx-verde">
+      <div class="grid-column-1 m-y-1 gobmx-texto-oscuro-1">
         <p>color de texto</p>
       </div>
       <div class="grid-column-3">
-        <p>.texto-gobmx-verde</p>
-        <p>#1F5D50</p>
+        <p>.gobmx-texto-oscuro-1</p>
       </div>
 
-      <div class="grid-column-1 m-y-1 texto-gobmx-pino">
+      <div class="grid-column-1 m-y-1 gobmx-texto-oscuro-2">
         <p>color de texto</p>
       </div>
       <div class="grid-column-3">
-        <p>.texto-gobmx-pino</p>
-        <p>#18302D</p>
+        <p>.gobmx-texto-oscuro-2</p>
       </div>
-
-      <div class="grid-column-1 m-y-1 texto-gobmx-beige">
+      <div class="grid-column-1 m-y-1 gobmx-texto-claro-1">
         <p>color de texto</p>
       </div>
       <div class="grid-column-3">
-        <p>.texto-gobmx-beige</p>
-        <p>#DFC9A1</p>
+        <p>.gobmx-texto-claro-1</p>
       </div>
-
-      <div class="grid-column-1 m-y-1 texto-gobmx-dorado">
+      <div class="grid-column-1 m-y-1 gobmx-texto-claro-2">
         <p>color de texto</p>
       </div>
       <div class="grid-column-3">
-        <p>.texto-gobmx-dorado</p>
-        <p>#BD9558</p>
+        <p>.gobmx-texto-claro-2</p>
       </div>
-
-      <div class="grid-column-1 m-y-1 texto-gobmx-tinto">
+      <div class="grid-column-1 m-y-1 gobmx-texto-acento">
         <p>color de texto</p>
       </div>
       <div class="grid-column-3">
-        <p>.texto-gobmx-tinto</p>
-        <p>#9E2148</p>
+        <p>.gobmx-texto-acento</p>
       </div>
     </div>
 
     <h2>Identidad Conacyt</h2>
     <div class="grid">
-      <div class="grid-column-1 m-y-1 fondo-conacyt-azul" style="border-radius: 8px;"></div>
+      <div class="grid-column-1 m-y-1 conacyt-fondo-oscuro-1" style="border-radius: 8px;"></div>
       <div class="grid-column-3">
-        <p>.fondo-conacyt-azul</p>
-        <p>#022562</p>
+        <p>.conacyt-fondo-oscuro-1</p>
+        <p>#001c4a</p>
       </div>
-      <div class="grid-column-1 m-y-1 fondo-conacyt-gris" style="border-radius: 8px;"></div>
+      <div class="grid-column-1 m-y-1 conacyt-fondo-oscuro-2" style="border-radius: 8px;"></div>
       <div class="grid-column-3">
-        <p>.fondo-conacyt-gris</p>
-        <p>#8C8D8E</p>
+        <p>.conacyt-fondo-oscuro-2</p>
+        <p>#002663</p>
       </div>
-      <div class="grid-column-1 m-y-1 fondo-conacyt-degradado" style="border-radius: 8px;"></div>
+      <div class="grid-column-1 m-y-1 conacyt-fondo-claro-1" style="border-radius: 8px;"></div>
       <div class="grid-column-3">
-        <p>.fondo-conacyt-degradado</p>
+        <p>.conacyt-fondo-claro-1</p>
+        <p>#fff</p>
+      </div>
+      <div class="grid-column-1 m-y-1 conacyt-fondo-claro-2" style="border-radius: 8px;"></div>
+      <div class="grid-column-3">
+        <p>.conacyt-fondo-claro-2</p>
+        <p>#d5d5d5</p>
+      </div>
+      <div class="grid-column-1 m-y-1 conacyt-fondo-acento" style="border-radius: 8px;"></div>
+      <div class="grid-column-3">
+        <p>.conacyt-fondo-acento</p>
+        <p>#13aff0</p>
+      </div>
+      <div class="grid-column-1 m-y-1 conacyt-fondo-degradado" style="border-radius: 8px;"></div>
+      <div class="grid-column-3">
+        <p>.conacyt-fondo-degradado</p>
         <p>#003384, #002663, #001c4a</p>
       </div>
     </div>
     <div class="grid">
-      <div class="grid-column-1 m-y-1 borde-conacyt-azul" style="border-width: 4px; border-style: solid; border-radius: 8px;"></div>
+      <div class="grid-column-1 m-y-1 conacyt-borde-oscuro-1" style="border-width: 4px; border-style: solid; border-radius: 8px;"></div>
       <div class="grid-column-3">
-        <p>.borde-conacyt-azul</p>
-        <p>#022562</p>
+        <p>.conacyt-borde-oscuro-1</p>
       </div>
-      <div class="grid-column-1 m-y-1 borde-conacyt-gris" style="border-width: 4px; border-style: solid; border-radius: 8px;"></div>
+      <div class="grid-column-1 m-y-1 conacyt-borde-oscuro-2" style="border-width: 4px; border-style: solid; border-radius: 8px;"></div>
       <div class="grid-column-3">
-        <p>.borde-conacyt-gris</p>
-        <p>#8C8D8E</p>
+        <p>.conacyt-borde-oscuro-2</p>
+      </div>
+      <div class="grid-column-1 m-y-1 conacyt-borde-claro-1" style="border-width: 4px; border-style: solid; border-radius: 8px;"></div>
+      <div class="grid-column-3">
+        <p>.conacyt-borde-claro-1</p>
+      </div>
+      <div class="grid-column-1 m-y-1 conacyt-borde-claro-2" style="border-width: 4px; border-style: solid; border-radius: 8px;"></div>
+      <div class="grid-column-3">
+        <p>.conacyt-borde-claro-2</p>
+      </div>
+      <div class="grid-column-1 m-y-1 conacyt-borde-acento" style="border-width: 4px; border-style: solid; border-radius: 8px;"></div>
+      <div class="grid-column-3">
+        <p>.conacyt-borde-acento</p>
       </div>
     </div>
     <div class="grid">
-      <div class="grid-column-1 m-y-1 texto-conacyt-azul">
+      <div class="grid-column-1 m-y-1 conacyt-texto-oscuro-1">
         <p>Color de texto</p>
       </div>
       <div class="grid-column-3">
-        <p>.texto-conacyt-azul</p>
-        <p>#022562</p>
+        <p>.conacyt-texto-oscuro-1</p>
       </div>
-      <div class="grid-column-1 m-y-1 texto-conacyt-gris">
+      <div class="grid-column-1 m-y-1 conacyt-texto-oscuro-2">
         <p>Color de texto</p>
       </div>
       <div class="grid-column-3">
-        <p>.texto-conacyt-gris</p>
-        <p>#8C8D8E</p>
+        <p>.conacyt-texto-oscuro-2</p>
+      </div>
+      <div class="grid-column-1 m-y-1 conacyt-texto-claro-1">
+        <p>Color de texto</p>
+      </div>
+      <div class="grid-column-3">
+        <p>.conacyt-texto-claro-1</p>
+      </div>
+      <div class="grid-column-1 m-y-1 conacyt-texto-claro-2">
+        <p>Color de texto</p>
+      </div>
+      <div class="grid-column-3">
+        <p>.conacyt-texto-claro-2</p>
+      </div>
+      <div class="grid-column-1 m-y-1 conacyt-texto-acento">
+        <p>Color de texto</p>
+      </div>
+      <div class="grid-column-3">
+        <p>.conacyt-texto-acento</p>
       </div>
     </div>
 
-    <h2>Variables de color de texto</h2>
+    <h2 class="m-t-12">Variables de color de texto</h2>
     <div class="grid">
       <div class="grid-column-1 m-y-1 texto-color-primario">
         <p>Color de texto</p>
       </div>
       <div class="grid-column-3">
         <p>
-          .texto-color-primario <br>
+          .texto-color-primario<br>
           var(--tipografia-color)
         </p>
         <p>#141414</p>
--- docs/src/views/HipervinculosView.vue
+++ docs/src/views/HipervinculosView.vue
@@ -2,17 +2,17 @@
   <div class="contenedor m-y-12">
     <h1>Hipervinculos</h1>
     <p>
-      <a href="#">a link</a>
-      <button class="hipervinculo">button.hipervinculo</button>
-      <a href="#">a link <span class="icon dai-descargar"></span></a>
-      <button class="hipervinculo">button.hipervinculo <span class="icon dai-descargar"></span></button>
+      <a href="#">etiqueta de hipervinculo a</a> o 
+      <button class="hipervinculo">button.hipervinculo</button> o 
+      <a href="#">a <span class="icon dai-descargar"></span></a> o 
+      <button class="hipervinculo">button.hipervinculo <span class="icon dai-descargar"></span></button> 
     </p>
     <h2>Identidad</h2>
-    <p class="fondo-gobmx-pino p-3">
+    <p class="gobmx-fondo-oscuro-1 p-3">
       <a href="#" class="hipervinculo-gobmx m-r-3">a.hipervinculo-gobmx</a>
       <button class="hipervinculo-gobmx">button.hipervinculo-gobmx</button>
     </p>
-    <p class="fondo-conacyt-azul p-3">
+    <p class="conacyt-fondo-oscuro-1 p-3">
       <a href="#" class="hipervinculo-conacyt m-r-3">a.hipervinculo-conacyt</a>
       <button class="hipervinculo-conacyt">button.hipervinculo-conacyt</button>
     </p>
