745cb97ee86dc04f7b25fcc1de8c7b3257eb8068
flkt <flkt.crnpio@gmail.com>
2022-07-14 15:36:48 -0500

    terminando estilos de navegacion, principal, de conacyt y de gobierno

---
+++ docs/src/views/NavegacionView.vue
@@ -0,0 +1,118 @@
+<template>
+
+  <h1>Navegación</h1>
+
+  <nav class="navegacion">
+    <div class="nav-contenedor-identidad">
+      <a href="#" class="nav-hiperviculo-logo">
+        <svg class="nav-logo" id="logo-14" width="60" height="38" viewBox="0 0 73 49" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M46.8676 24C46.8676 36.4264 36.794 46.5 24.3676 46.5C11.9413 46.5 1.86765 36.4264 1.86765 24C1.86765 11.5736 11.9413 1.5 24.3676 1.5C36.794 1.5 46.8676 11.5736 46.8676 24Z" class="ccustom" fill="#68DBFF"></path> <path d="M71.1324 24C71.1324 36.4264 61.1574 46.5 48.8529 46.5C36.5484 46.5 26.5735 36.4264 26.5735 24C26.5735 11.5736 36.5484 1.5 48.8529 1.5C61.1574 1.5 71.1324 11.5736 71.1324 24Z" class="ccompli1" fill="#FF7917"></path> <path d="M36.6705 42.8416C42.8109 38.8239 46.8676 31.8858 46.8676 24C46.8676 16.1144 42.8109 9.17614 36.6705 5.15854C30.5904 9.17614 26.5735 16.1144 26.5735 24C26.5735 31.8858 30.5904 38.8239 36.6705 42.8416Z" class="ccompli2" fill="#5D2C02"></path> </svg>
+      </a>
+      <button class="nav-boton-menu">
+        <span class="nav-icono-menu"></span>
+      </button>
+      <div class="nav-informacion">
+        Nombre de la sección actual
+      </div>
+    </div>
+    <div class="nav-menu-contedor abierto">
+      <div class="nav-menu-complementario">
+        <a href="#" class="nav-hipervinculo">Menú externo</a>
+        <button class="nav-boton">nav boton</button>
+      </div>
+      <div class="nav-menu-principal">
+        <ul class="nav-menu">
+          <li><a href="#" class="nav-hipervinculo">Menú</a></li>
+          <li class="nav-contenedor-submenu">
+            <button class="nav-boton-submenu"> <span class="icon dai-persona m-r-1"></span> Menú con submenu</button>
+            <ul class="nav-submenu">
+              <li><button class="nav-boton-regresar">Menú con submenu</button></li>
+              <li><a href="#" class="nav-hipervinculo">Menú del submenu pues en teoria se estira</a></li>
+              <li><a href="#" class="nav-hipervinculo">Menú del submenu</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="#" class="nav-hipervinculo">
+              <span class="icon dai-persona m-r-1"></span> 
+              Menú
+            </a>
+          </li>
+          <li><a href="#" class="nav-hipervinculo">Menú</a></li>
+          <li><a href="#" class="nav-hipervinculo">Menú</a></li>
+          <li>
+            <button class="nav-boton">nav boton</button>
+          </li>
+          <li>
+            <button class="nav-boton-icon">
+              <span class="icon dai-persona"></span> 
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  
+  <h2 class="m-t-10">Identidad</h2>
+  <h3>Gobierno</h3>
+  <nav class="navegacion navegacion-gobmx">
+    <div class="nav-contenedor-identidad">
+      <a href="https://www.gob.mx/" class="nav-hiperviculo-logo" target="_blank" rel="noopener">
+        <img width="128" height="38" class="nav-logo" src="https://framework-gb.cdn.gob.mx/landing/img/logoheader.svg" alt="Gobierno de México.">
+      </a>
+      <button class="nav-boton-menu">
+        <span class="nav-icono-menu"></span>
+      </button>
+    </div>
+    <div class="nav-menu-contedor">
+      <div class="nav-menu-principal">
+        <ul class="nav-menu">
+          <li><a href="https://mivacuna.salud.gob.mx/index.php" class="nav-hipervinculo" target="_blank" rel="noopener">Registro para vacunación</a></li>
+          <li><a href="https://coronavirus.gob.mx/" class="nav-hipervinculo" target="_blank" rel="noopener">Información sobre COVID-19</a></li>
+          <li><a href="https://www.gob.mx/tramites" class="nav-hipervinculo" target="_blank" rel="noopener">Trámites</a></li>
+          <li><a href="https://www.gob.mx/gobierno" class="nav-hipervinculo" target="_blank" rel="noopener">Gobierno</a></li>
+          <li><a href="https://www.gob.mx/busqueda" class="nav-hipervinculo" target="_blank" rel="noopener"><span class="icon dai-buscar"></span> </a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <h3 class="m-t-10">Navegación de Conacyt</h3>
+  <nav class="navegacion navegacion-conacyt">
+    <div class="nav-contenedor-identidad">
+      <a href="#" class="nav-hiperviculo-logo">
+        <img width="130" height="38" src="https://conacyt.mx/wp-content/uploads/2021/10/logo_conacyt_con_sintagma_azul_completo.svg" alt="Conacyt.">
+      </a>
+      <button class="nav-boton-menu">
+        <span class="nav-icono-menu"></span>
+      </button>
+      <div class="nav-informacion">
+        Nombre de la sección actual
+      </div>
+    </div>
+    <div class="nav-menu-contedor">
+      <div class="nav-menu-complementario">
+        <a href="#" class="nav-hipervinculo">Menú externo</a>
+      </div>
+      <div class="nav-menu-principal">
+        <ul class="nav-menu">
+          <li><a href="#" class="nav-hipervinculo">Menú</a></li>
+          <li class="nav-contenedor-submenu">
+            <button class="nav-boton-submenu"> Menú con submenu</button>
+            <ul class="nav-submenu">
+              <li><button class="nav-boton-regresar">Menú con submenu</button></li>
+              <li><a href="#" class="nav-hipervinculo">Menú del submenu pues en teoria se estira</a></li>
+              <li><a href="#" class="nav-hipervinculo">Menú del submenu</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="#" class="nav-hipervinculo">Menú</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <h2 class="m-t-10">Navegación</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, exercitationem. Aliquid, totam mollitia earum laudantium, quo vitae tempore, officia iusto illo debitis voluptas obcaecati vel aperiam odio perspiciatis incidunt possimus!</p>
+
+
+</template>
