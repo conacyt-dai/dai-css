8f4767d90079f7a565393190a22fdfbe84eb5703
flkt <flkt.crnpio@gmail.com>
2022-07-26 16:22:52 -0500

    agregando texto de introduccion

--- docs/src/views/InicioView.vue
+++ docs/src/views/InicioView.vue
@@ -1,5 +1,30 @@
 <template>
-
-  <h1>inicio</h1>
+  <div class="contenedor ancho-lectura">
+    <h1>Hola!</h1>
+    <p class="parrafo-texto-alto">
+      Esta es una biblioteca de puros puritos estilos de css.
+      En teoría aquí deben escribirse los estilos generales de etiquetas de HTML, clases y
+      variables que sean indisplensables para visualizar todos los sitios,
+      agregarles accesibilidad y usabilidad.
+    </p>
+    <p class="parrafo-texto-alto">
+      Nuestro sistema de diseño está basado en la organización del 
+      <a href="https://atomicdesign.bradfrost.com/table-of-contents/" target="_blank" rel="noopener">atomic design</a>
+      de Brad Frost.
+    </p>
+    <p class="parrafo-texto-alto">
+      Pueden leer este link de referencia para saber un poco más de la
+      <a href="https://designhandbook.mendesaltaren.com/design-systems" target="_blank" rel="noopener">definición del sistema de diseño</a>.
+      Personalmente me gusto mucho ese documento, además que está en español y está alineado a lo que llevamos diseñado y desarrollado.
+      ( Al menos en teoría, jeje )
+    </p>
+    <p class="parrafo-texto-alto">
+      Espero se diviertan mucho usandola y actualizandola.
+    </p>
+    <p class="notas m-t-maximo">
+      <hr>
+      Primer versión hecha con cariño por su <span class="icono-flkt"></span>
+    </p>
+  </div>
 
 </template>
